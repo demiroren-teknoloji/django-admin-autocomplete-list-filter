da8decc87a8bbcf818f54d69a449ca65c58a679c
Uğur Özyılmazel <ugur.ozyilmazel@demirorenteknoloji.com>
2019-10-25 10:21:51 +0300

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(CURRENT_WORKING_DIRECTORY, 'README.md')) as fp:
 
 setup(
     name='django-admin-autocomplete-list-filter',
-    version='0.1.2',
+    version='0.1.3',
     description='Ajax autocomplete list filter for Django admin',
     long_description=README,
     long_description_content_type='text/markdown',
